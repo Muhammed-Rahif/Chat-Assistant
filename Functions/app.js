793f9b16e7c175a13c9e7c47de5f745d4e63946c
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-01 16:25:46 +0530

    Chat assistant simple application finished...

--- Functions/app.js
+++ Functions/app.js
@@ -1,5 +1,36 @@
+function respondBot(msg) {
+    msg.toLowerCase()
+  if (msg === "hi") {
+    return "Hello!";
+  } else if (msg === "") {
+    return "type something...!!!";
+  } else if (msg === "hi how are you?") {
+    return "Yeah..I am good! What about you?";
+  } else if (msg === "Good") {
+    return ":-D";
+  }else if (msg === "who developed you?") {
+    return "That's a boy called 'Rahif'";
+  }else if (msg === "Hello") {
+    return "Hey!";
+  }else if (msg === "who developed you?") {
+    return "That's a boy called 'Rahif'";
+  }
+  
+  
+  
+  else {
+    return "I am confused...!";
+  }
+}
+
 function sendMsg() {
-    var now=moment().format('MMMM Do YYYY, h:mm:ss a');
-    var msg=document.getElementById("text-message").value
-    alert("Send msg :"+msg+"Now date :"+now)
+  var time = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var msg = document.getElementById("text-message").value;
+
+  document.getElementById("user-chat").innerHTML = msg;
+
+  let response = respondBot(msg);
+  document.getElementById("bot-chat").innerHTML = response;
+
+
 }
