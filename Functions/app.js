31ac1d635f24eddcb3e4bdb07511185a6f84c316
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-01-04 17:04:53 +0530

    Fixes some bugs, Javascript text to speech librery(Talkify) included

--- Functions/app.js
+++ Functions/app.js
@@ -1,27 +1,29 @@
 function respondBot(msg) {
-    msg.toLowerCase()
+  msg=msg.toLowerCase();
   if (msg === "hi") {
     return "Hello!";
   } else if (msg === "") {
     return "type something...!!!";
   } else if (msg === "hi how are you?") {
-    return "Yeah..I am good! What about you?";
-  } else if (msg === "Good") {
+    return "Yeah,I am good! What about you?";
+  } else if (msg === "good") {
     return ":-D";
-  }else if (msg === "who developed you?") {
+  } else if (msg === "who developed you?") {
     return "That's a boy called 'Rahif'";
-  }else if (msg === "Hello") {
+  } else if (msg === "Hello") {
     return "Hey!";
-  }else if (msg === "who developed you?") {
-    return "That's a boy called 'Rahif'";
-  }
-  
+  } 
   
   
   else {
     return "I am confused...!";
   }
 }
+
+$(document).ready(()=>{
+  $('form').submit(false);
+  startSpeech();
+})
 
 function sendMsg() {
   var time = moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -32,5 +34,21 @@ function sendMsg() {
   let response = respondBot(msg);
   document.getElementById("bot-chat").innerHTML = response;
 
+  textSpeech();
+}
 
+function textSpeech() {
+  var player = new talkify.Html5Player();
+  var msg = document.getElementById("text-message").value;
+  let text = respondBot(msg);
+  player.playText(text);
+}
+
+
+
+function startSpeech() {
+  var player = new talkify.Html5Player();
+  var msg = document.getElementById("text-message").value;
+  let text = respondBot(msg);
+  player.playText("Hai I am Chat bot. Say 'Hi' to me...!");
 }
